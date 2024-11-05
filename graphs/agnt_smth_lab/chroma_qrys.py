63b5b67cbf1dc561b9be461c9a55a721e81783a7
stiproot <stipcich.simon@gmail.com>
2024-11-05 07:51:02 +0200

    chore: aligning with latest agnt_smth functionality.

--- graphs/agnt_smth_lab/chroma_qrys.py
+++ graphs/agnt_smth_lab/chroma_qrys.py
@@ -10,12 +10,8 @@ from langgraph.prebuilt.tool_executor import ToolExecutor
 
 from agnt_smth.core.utls import log, ModelFactory, ChromaHttpClientFactory, EmbeddingFactory, RetrieverFactory
 from agnt_smth.core.tools import RetrieveAdditionalContextTool
-from agnt_smth.core.agnts import (
-    create_agent_executor,
-    should_invoke_tools,
-    invoke_tools,
-    AgentState,
-)
+from agnt_smth.core.agnts import AgentState
+from agnt_smth.core.workflows import build_agnt_with_tools_graph
 
 SYS_PROMPT = """
     You are a helpful agent with expertise in answering technical questions about a code repository.
@@ -31,53 +27,24 @@ def build_graph(repo_name: str):
 
     chroma_client = ChromaHttpClientFactory.create_with_auth()
     embedding_function = EmbeddingFactory.create()
-    retriever = RetrieverFactory.create(collection_name=repo_name, chroma_client=chroma_client, embedding_function=embedding_function)
-    context_retriever = RetrieveAdditionalContextTool(repo_name, retriever=retriever)
+    retriever = RetrieverFactory.create(repo_name, chroma_client=chroma_client, embedding_function=embedding_function)
+    print("retriever type", type(retriever))
+    context_retriever = RetrieveAdditionalContextTool(retriever, collection_name=repo_name)
 
     tools = [context_retriever]
-    tool_executor = ToolExecutor(tools)
 
-    prompt = ChatPromptTemplate.from_messages(
-        [("system", SYS_PROMPT), MessagesPlaceholder(variable_name="messages")]
-    )
+    graph = build_agnt_with_tools_graph(sys_prompt=SYS_PROMPT, tools=tools)
 
-    model = ModelFactory.create().bind_tools(tools)
-    chain = prompt | model
-    agent_node = create_agent_executor(chain=chain)
+    return graph
 
-    graph = StateGraph(AgentState)
-
-    graph.add_node("agent", agent_node)
-    graph.add_node(
-        "invoke_tools", functools.partial(invoke_tools, tool_executor=tool_executor)
-    )
-
-    graph.add_edge(START, "agent")
-
-    graph.add_conditional_edges(
-        "agent",
-        should_invoke_tools,
-        {
-            "invoke_tools": "invoke_tools",
-            "continue": END,
-        },
-    )
-
-    graph.add_edge("invoke_tools", "agent")
-    graph.add_edge("agent", END)
-
-    return graph.compile()
 
 
 if __name__ == "__main__":
+
     repo_name = "Internal-Lexi"
+    inputs = {"messages": [HumanMessage(content="What is Lexi? How can I run it?")]}
 
-    graph = build_graph(repo_name)
-
-    inputs = {
-        "messages": [HumanMessage(content=USER_INPUT)],
-        "user_input": "What can you tell me about Lexi?",
-    }
+    app = build_graph(repo_name)
 
     for output in app.stream(inputs):
         for key, value in output.items():
